b9abcb12a63782eff3beb4ead602280f8682dd2b
Goatz <Goataragua@gmail.com>
2025-05-14 17:30:31 +0300

    added route for card search through API

--- backend/routes/card.route.js
+++ backend/routes/card.route.js
@@ -6,12 +6,16 @@ const {
     addToCollection,
     updateCard,
     deleteCard,
+    getCardFromAPI
 } = require('../controllers/card.controller');
 
 router.get('/', getAllCards); // Get all cards
-router.get('/:name', getCardByName); // Get card by name
+router.get('/:name', getCardByName); // Get card by name from DB
 router.post('/', addToCollection); // Add card to collection
 router.put('/:id', updateCard); // Update card in collection
 router.delete('/:id', deleteCard); // Delete card from collection
+
+// MTG API Routes
+router.get('/search/:name', getCardFromAPI); // Get card by name from API
 
 module.exports = router;
