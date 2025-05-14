8ea99a9cd2cd815b9460674daae6dd3f488d77e2
Goatz <Goataragua@gmail.com>
2025-05-14 18:32:08 +0300

    added logs for server connection

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,15 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
+
+// Connection check
+const  checkConnection = () => {
+    const db = mongoose.connection;
+    console.log('MongoDB connection state:', db.states[db.readyState]);
+    console.log('Connected to database:', db.name);
+    console.log('Connected to host:', db.host);
+    console.log('Connected to port:', db.port);
+};
+
 const app = require('./app');
 
 const port = process.env.PORT || 4000;
@@ -9,6 +19,7 @@ mongoose.connect(process.env.MONGODB_URI)
     .then(
         () => {
             console.log("Connection to MongoDB established");
+            checkConnection();
             app.listen(port, () => {
                 console.log(`Server running on port ${port}`);
             });
