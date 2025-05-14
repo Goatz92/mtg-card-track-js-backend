552c49f29d9702fd05d02ecd8e376a5029b74efc
Goatz <Goataragua@gmail.com>
2025-05-14 19:38:59 +0300

    clarified routes

--- backend/routes/card.route.js
+++ backend/routes/card.route.js
@@ -14,7 +14,7 @@ router.get('/search/:name', getCardFromAPI); // Get card by name from API
 
 router.get('/', getAllCards); // Get all cards
 router.post('/add', addToCollection); // Add card to collection
-router.get('/:name', getCardByName); // Get card by name from DB
+router.get('/:name', getCardByName); // Get card by name from collection
 router.put('/:id', updateCard); // Update card in collection
 router.delete('/:id', deleteCard); // Delete card from collection
 
