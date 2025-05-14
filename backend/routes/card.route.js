b5ee77189890d55d071faf24c858a3b63b93f332
Goatz <Goataragua@gmail.com>
2025-05-14 18:30:13 +0300

    updated routes for clarity

--- backend/routes/card.route.js
+++ backend/routes/card.route.js
@@ -9,13 +9,13 @@ const {
     getCardFromAPI
 } = require('../controllers/card.controller');
 
-router.get('/', getAllCards); // Get all cards
-router.get('/:name', getCardByName); // Get card by name from DB
-router.post('/', addToCollection); // Add card to collection
-router.put('/:id', updateCard); // Update card in collection
-router.delete('/:id', deleteCard); // Delete card from collection
-
 // MTG API Routes
 router.get('/search/:name', getCardFromAPI); // Get card by name from API
+
+router.get('/', getAllCards); // Get all cards
+router.post('/add', addToCollection); // Add card to collection
+router.get('/:name', getCardByName); // Get card by name from DB
+router.put('/:id', updateCard); // Update card in collection
+router.delete('/:id', deleteCard); // Delete card from collection
 
 module.exports = router;
