8bae40e9cbc2105e0be218c19bca933a6d007c38
Goatz <Goataragua@gmail.com>
2025-05-16 17:39:40 +0300

    Update change API to Scryfall API and implement methods for search & add cards

--- backend/controllers/card.controller.js
+++ backend/controllers/card.controller.js
@@ -2,23 +2,34 @@ const cardService = require('../services/card.service');
 const logger = require ('../logger/logger');
 const { successResponse, errorResponse } = require('../utils/responseHandler');
 
-// @desc    Search for cards in MTG API by name
-// @route   GET /api/cards/search/:name
-// @access  Public
-const getCardFromAPI = async (req, res) => {
+// @desc Search Scryfall for a card
+// @route GET /search/:name
+const searchCardScryfall = async (req, res) => {
     try {
-        const card = await cardService.searchMTGAPI(req.params.name);
-        if (!card || card.length === 0) {
-            return errorResponse(res, 404, 'Card not found');
-        }
-        successResponse(res, 200, card, 'Card retrieved successfully');
+        const cards = await cardService.searchScryfallAPI(req.params.name);
+        res.json(cards);
     } catch (error) {
-        logger.error('Error searcing MTG API', { error: error.message});
-        errorResponse(res, 500, 'Error fetching cards from API', error);
+        res.status(500).json({ error: message });
     }
-} 
+}
 
-// @desc    Get all cards
+// @desc    Add card From Scryfall
+// @route   GET /api/cards/scryfall/:scryfallId
+// @access  Public
+const addCardFromScryfall = async (req, res) => {
+    try {
+        const { scryfallId } = req.params;
+        const userData = req.body;
+
+        const card = await cardService.addCardFromScryfall(scryfallId, userData);
+
+        successResponse(res, 201, card, 'Card added succesfully');
+    } catch (error) {
+        errorResponse(res, 500, 'Error adding card', error);
+    }
+};
+
+// @desc    Get all cards from database
 // @route   GET /api/cards
 // @access  Public
 const getAllCards = async (req, res) => {
@@ -48,25 +59,6 @@ const getCardByName = async (req, res) => {
         errorResponse(res, 500, 'Error fetching card', error);
     }
 }
-
-// @desc    Add a card to user's collection
-// @route   POST /api/collection
-// @access  Public
-const addToCollection = async (req, res) => {
-    try {
-        const savedCard = await cardService.addCard(req.body);
-        successResponse(res, 201, savedCard, 'Card added to collection succesfully');
-    } catch (error) {
-        logger.error('Error adding card', {
-            cardData: req.body,
-            error: error.message
-        });
-
-        const status = error.message.includes('not found') ? 404 : 500;
-        errorResponse(res, status, error.message, error);
-    }
-}
-
 
 // @desc    Update a card in user's collection
 // @route   PUT /api/collection/:id
@@ -114,8 +106,44 @@ const deleteCard = async (req, res) => {
 module.exports = {
     getAllCards,
     getCardByName,
-    addToCollection,
     updateCard,
     deleteCard,
-    getCardFromAPI
+    searchCardScryfall,
+    addCardFromScryfall
 };
+
+// @desc    Search for cards in MTG API by name
+// @route   GET /api/cards/search/:name
+// @access  Public
+// const getCardFromAPI = async (req, res) => {
+//     try {
+//         const card = await cardService.searchMTGAPI(req.params.name);
+//         if (!card || card.length === 0) {
+//             return errorResponse(res, 404, 'Card not found');
+//         }
+//         successResponse(res, 200, card, 'Card retrieved successfully');
+//     } catch (error) {
+//         logger.error('Error searcing MTG API', { error: error.message});
+//         errorResponse(res, 500, 'Error fetching cards from API', error);
+//     }
+// } 
+
+
+// @desc    Add a card to user's collection
+// @route   POST /api/collection
+// @access  Public
+// const addToCollection = async (req, res) => {
+//     try {
+//         const savedCard = await cardService.addCard(req.body);
+//         successResponse(res, 201, savedCard, 'Card added to collection succesfully');
+//     } catch (error) {
+//         logger.error('Error adding card', {
+//             cardData: req.body,
+//             error: error.message
+//         });
+
+//         const status = error.message.includes('not found') ? 404 : 500;
+//         errorResponse(res, status, error.message, error);
+//     }
+// }
+
