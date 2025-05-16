9e7d446ba4464d38376f39ba21d4b68686a2892e
Goatz <Goataragua@gmail.com>
2025-05-16 14:59:06 +0300

    add name decoder on getCardByName function

--- backend/controllers/card.controller.js
+++ backend/controllers/card.controller.js
@@ -37,7 +37,7 @@ const getAllCards = async (req, res) => {
 // @access  Public
 const getCardByName = async (req, res) => {
     try {
-        // const decodedName = decodeURIComponent(req.params.name);
+        const decodedName = decodeURIComponent(req.params.name);
         const card = await cardService.findByName(decodedName);
 
         if(!card) {
