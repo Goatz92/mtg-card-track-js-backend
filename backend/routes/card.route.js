47d1439e8b02a9fd763e2408444520336738c66b
Goatz <Goataragua@gmail.com>
2025-05-16 17:41:16 +0300

    build route paths for ScryfallAPI implementation

--- backend/routes/card.route.js
+++ backend/routes/card.route.js
@@ -3,18 +3,24 @@ const router = express.Router();
 const { 
     getAllCards,
     getCardByName,
-    addToCollection,
     updateCard,
     deleteCard,
-    getCardFromAPI
+    searchCardScryfall,
+    addCardFromScryfall
 } = require('../controllers/card.controller');
 
-// MTG API Routes
-router.get('/search/:name', getCardFromAPI); // Get card by name from API
+// Scryfall routes
+router.get('/search/:name', searchCardScryfall) // Search for single card in Scryfall
+router.post('/scryfall/:scryfallId', addCardFromScryfall); // Add a card from Scryfall to collection
 
+//MTG API routes
+// router.get('/search/:name', getCardFromAPI); // Get card by name from API
+
+//Local DB operations
 router.get('/', getAllCards); // Get all cards
-router.post('/add', addToCollection); // Add card to collection
 router.get('/:name', getCardByName); // Get card by name from collection
+
+//Collection Managment
 router.put('/:id', updateCard); // Update card in collection
 router.delete('collection/:id', deleteCard); // Delete card from collection
 
