12093d47afcad17dcab8958faff1abe1c52b4659
Goatz <Goataragua@gmail.com>
2025-05-28 17:03:25 +0300

    chore: tidy up cardController method calls, fix: change scryfall routes because of route conflict from using the same path

--- backend/routes/card.route.js
+++ backend/routes/card.route.js
@@ -1,29 +1,21 @@
 const express = require('express');
 const router = express.Router();
-const { 
-    getAllCards,
-    getCardByName,
-    updateCard,
-    deleteCard,
-    searchCardScryfall,
-    addCardFromScryfall
-} = require('../controllers/card.controller');
-const { addCardFromScryfallByName } = require('../services/card.service');
+const cardController = require('../controllers/card.controller');
 
 // Scryfall routes
-router.get('/search/:name', searchCardScryfall);
-router.post('/scryfall/:scryfallId', addCardFromScryfall); 
-router.post('/scryfall/:name', addCardFromScryfallByName);
+router.get('/search/:name', cardController.searchCardScryfall);
+router.post('/scryfall/:scryfallId', cardController.addCardFromScryfall); 
+router.post('/scryfall/name/:cardName', cardController.addCardFromScryfallByName);
 
 //MTG API routes
 // router.get('/search/:name', getCardFromAPI); // Get card by name from API
 
 //Local DB operations
-router.get('/', getAllCards);
-router.get('/:name', getCardByName); 
+router.get('/', cardController.getAllCards);
+router.get('/:name', cardController.getCardByName); 
 
 //Collection Managment
-router.put('/:id', updateCard); 
-router.delete('collection/:id', deleteCard); 
+router.put('/:id', cardController.updateCard); 
+router.delete('collection/:id', cardController.deleteCard); 
 
 module.exports = router;
