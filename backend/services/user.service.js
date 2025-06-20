65e826880728cd3894ad6bd5454dc6199bb6275a
Goatz <Goataragua@gmail.com>
2025-06-20 12:50:22 +0300

    Feat: add methods to add and remove cards

--- backend/services/user.service.js
+++ backend/services/user.service.js
@@ -1,4 +1,5 @@
 const User = require('../models/user.model');
+const Card = require('../models/card.model');   
 const logger = require('../logger/logger');
 
 async function findAll() {
@@ -34,8 +35,84 @@ async function findLastInserted() {
     }
 }
 
+async function addCardToCollection(username, cardId, quantity = 1) {
+    try {
+        // Verify card exists
+        const card = await Card.findById(cardId);
+        if(!card) {
+            logger.error('Card not found', { cardId });
+            throw new Error('Card not found');
+        }
+        // Update user collection
+        const user = await User.findOneAndUpdate(
+            { username },
+            {
+                $addToSet: {
+                    'collection.cards' : {
+                        card: cardId,
+                        quantity: quantity
+                    }
+                },
+                $inc: { 'collection.totalCards': quantity }
+            },
+            { new: true }
+        );
+
+        if(!user) {
+            logger.error('User not found', { username });
+            throw new Error('User not found');
+        }
+
+        logger.debug(`Added card ${cardId} to ${username}'s collection`, { quantity })
+        return user;
+    } catch (error) {
+        logger.error('Error adding card to collection', {
+            username,
+            cardId,
+            error: error.message
+        });
+    }
+}
+
+async function removeCardFromCollection(username, cardId) {
+    try {
+        const user = await User.findOne({ username });
+        if(!user) {
+            logger.error('User not found', { username });
+            throw new Error('User not found');
+        }
+        // Find card to get quantity before removal
+        const cardEntry = user.collection.cards.find(c => c.card.equals(cardId));
+        if (!cardEntry) {
+            logger.error('Card not found in collection', { username, cardId });
+            throw new Error('Card not found in Collection');
+        }
+
+        const updateUser = await User.findOneAndUpdate(
+            { username },
+            {
+                $pull: { 'collectionStats.cards': { card: cardId } },
+                $inc: { 'collection.totalCards': -cardEntry.quantity }
+            },
+            { new: true }
+        );
+
+        logger.debug(`Removed card ${cardId} from ${username}'s collection`);
+        return updatedUser;
+    } catch (error) {
+        logger.error('Error removing card from collection', {
+            username,
+            cardId,
+            error: error.message
+        });
+        throw error;
+    }
+}
+
 module.exports = { 
     findAll, 
     findOne, 
-    findLastInserted 
+    findLastInserted,
+    addCardToCollection,
+    removeCardFromCollection
 };
