6c66f9e3d906356d5c0c256e3db8e957daefc7ba
Goatz <Goataragua@gmail.com>
2025-07-11 01:04:46 +0300

    Add: base for deck functions

---
+++ backend/controllers/deck.controller.js
@@ -0,0 +1,19 @@
+const deckService = require('../services/deck.service');
+const logger = require('../logger/logger');
+const { successResponse, errorResponse } = require('../utils/responseHandler');
+
+const addCardToDeck = async (req, res) => {
+
+}
+
+const updateDeck = async(req,res) => {
+    
+}
+
+const deleteCardFromDeck = async(req, res) => {
+
+}
+
+const getAllDecks = async(req, res) => {
+    
+}
---
+++ backend/services/deck.service.js
@@ -0,0 +1,25 @@
+const Card = require('../models/card.model');
+const scryfallService = require('./scryfall.service');
+const logger = require('../logger/logger');
+const mongoose = require('mongoose');
+
+function addCardToDeck() {
+
+}
+
+function removeCardFromDeck() {
+
+}
+
+function deleteDeck() {
+
+}
+
+function searchAllDecks() {
+
+}
+
+function searchDeckByName() {
+
+}
+
